64e8c1768957f2e270b54f1770ab840441c84cbf
Sajal Chauhan <sajalchauhan2004@gmail.com>
2024-03-09 01:27:13 +0530

    docAI App configured

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include("authentication.urls"))
+    path("", include("authentication.urls")),
+    path("", include("docAI.urls"))
 ]
